5b9c3283933465ece87af5268c815c3f4d5de061
root <root@ip-172-31-47-79.ap-south-1.compute.internal>
2020-04-25 07:55:19 +0000

    aa

--- app.py
+++ app.py
@@ -63,3 +63,4 @@ if __name__ == "__main__":
 #    app.secret_key = os.urandom(12)
 #    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True,host='0.0.0.0', port=5000)
+    app.debug = True
